c6223ccb5b4168fc5becb05b206a13596165cce9
Wiktor Wojcik <wic97@tlen.pl>
2019-01-26 20:14:20 +0100

    Default Changelist

--- src/MMain.java
+++ src/MMain.java
@@ -4,8 +4,6 @@ import Core.Client.ServerOperation;
 import Core.ClientManager;
 import Core.Pracownik;
 
-import java.util.Date;
-
 public class MMain {
     public static void main(String[] args){
         System.out.println("dziadostwo pierdolone i taki chuj jak słonia nos");
@@ -13,9 +11,19 @@ public class MMain {
         Client client = new Client("localhost",4821);
         ClientManager clientManager = new ClientManager();
 
-        ClientManager.clientSender.sendToServer(ServerOperation.addPracownik,pr);
 
-        Pracownik pracownik = (Pracownik)ClientManager.clientSender.sendToServer(ServerOperation.getPracownik,2052);
-        System.out.println(pracownik.getImie());
+        //Atrakcje atrakcje = new Atrakcje(1,"jak",12,5,"2015.05.06","2020.05.06");
+        //ClientManager.clientSender.sendToServer(ServerOperation.addAttraction,atrakcje);
+
+        //Atrakcje atrakcje = (Atrakcje)ClientManager.clientSender.sendToServer(ServerOperation.getAttraction,2);
+        //System.out.println(atrakcje.getNazwa_atrakcji());
+
+        int ilosc = (Integer)ClientManager.clientSender.sendToServer(ServerOperation.howManyPracownik,null);
+        System.out.println(ilosc);
+
+        //ClientManager.clientSender.sendToServer(ServerOperation.addPracownik,pr);
+
+        //Pracownik pracownik = (Pracownik)ClientManager.clientSender.sendToServer(ServerOperation.getPracownik,2052);
+        //System.out.println(pracownik.getImie());
     }
 }
---
+++ src/java/park-rozrywki-desktop/parkrozrywki/EditUserScreen.java
@@ -0,0 +1,122 @@
+import Core.Client.Client;
+import Core.Client.ServerOperation;
+import Core.ClientManager;
+import Core.Pracownik;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+public class EditUserScreen extends JFrame implements ActionListener{
+
+    ArrayList<Pracownik> pracownicy;
+
+    JFrame frame = new JFrame("Eleden | Edit uzytkownik");
+
+    static final int width = 400;
+    static final int height = 600;
+
+    private JButton btnAdd;
+    private JButton btnReturn;
+
+    private JComboBox Prac;
+
+
+
+    /** prywatna zmienna typu JLabel - tlo aplikacji, w tym przypadku 400x600*/
+    protected JLabel background;
+    /** ikonka programu */
+    ImageIcon icon = new ImageIcon("src/resources/img/icon.png");
+
+    EditUserScreen(){
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        loadPracownicy();
+
+        String[] pr= new String[pracownicy.size()];
+
+        int i = -1;
+        for(Pracownik p:pracownicy){
+            i++;
+            pr[i] = new String(p.getLogin());
+        }
+
+
+        Prac = new JComboBox(pr);
+        Prac.setBounds(20,150,400,70);
+        Prac.addActionListener(this);
+        frame.add(Prac);
+
+
+        btnAdd = new JButton(new ImageIcon("src/resources/img/btnAdd.png"));
+        btnAdd.setBounds(20, 450, 160, 56);
+        btnAdd.setBorderPainted(false);
+        btnAdd.setContentAreaFilled(false);
+        btnAdd.addActionListener(this);
+        btnAdd.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                removeUserr(pr,Prac.getSelectedIndex());
+                JOptionPane.showMessageDialog(null, " uzytkownik zostal pomyslnie usuniety z systemu!", "Notyfikator", JOptionPane.INFORMATION_MESSAGE);
+                frame.dispose();
+                frame.setVisible(false);
+                new AdminScreen();
+            }
+        });
+        frame.add(btnAdd);
+
+        btnReturn = new JButton(new ImageIcon("src/resources/img/btnReturn.png"));
+        btnReturn.setBounds(210, 450, 160, 56);
+        btnReturn.setBorderPainted(false);
+        btnReturn.setContentAreaFilled(false);
+        btnReturn.addActionListener(this);
+        btnReturn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                frame.setVisible(false);
+                new AdminScreen();
+            }});
+        frame.add(btnReturn);
+
+        background = new JLabel(new ImageIcon("src/resources/img/background400x600.png"));
+        background.setOpaque(true);
+        background.setBounds(-10, 0, width, height);
+        frame.add(background);
+
+        frame.setVisible(true);
+        frame.setSize(width,height);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setIconImage(icon.getImage());
+    }
+
+    void loadPracownicy(){
+        Client client = new Client("localhost",4821);
+        ClientManager clientManager = new ClientManager();
+        int ilosc = (Integer)ClientManager.clientSender.sendToServer(ServerOperation.howManyPracownik,null);
+
+        pracownicy = (ArrayList<Pracownik>)ClientManager.clientSender.sendToServer(ServerOperation.getPracownikToList,null);
+
+    }
+
+    void removeUserr(String[] pr, int rem){
+        Client client = new Client("localhost",4821);
+        ClientManager clientManager = new ClientManager();
+
+
+        System.out.println(login);
+
+        ClientManager.clientSender.sendToServer(ServerOperation.removeUserFromBase,login);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+}
